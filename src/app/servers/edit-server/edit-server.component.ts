5d689332a17ac48fec024d3be8abc64a5c8eb967
okulkar2 <omkr22@gmail.com>
2017-09-17 01:47:01 -0700

    Query Parameters Used in routing

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -11,11 +11,17 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  editServer = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.queryParams.subscribe(
+      (queryparams: Params) => {
+        this.editServer = queryparams['allowEdit'] === '1' ? true : false;
+      }
+    );
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
